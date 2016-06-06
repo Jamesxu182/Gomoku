57e5e9dcf7a4d080305883b103f92a004d1c4bc3
pydo <964683112@qq.com>
2016-06-06 21:14:13 +0800

    添加注释

--- Point.py
+++ Point.py
@@ -2,7 +2,14 @@
 #-*- coding: utf-8 -*-
 
 class Point:
+
     def __init__(self, x, y):
+        '''
+        点,生成棋盘上的交点,并计算每个棋子多赢的实际像素坐标
+        棋盘坐标和像素坐标的转换
+        :param x:
+        :param y:
+        '''
         self.x = x;
         self.y = y;
         self.pixel_x = 30 + 30 * self.x
